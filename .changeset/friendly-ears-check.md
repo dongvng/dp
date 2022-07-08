e03b229945bca48ca238d60e489af9a695633495
Dong Nguyen <dongvn666@gmail.com>
2022-07-08 14:57:33 +0700

    init dp stack

---
+++ .changeset/friendly-ears-check.md
@@ -0,0 +1,9 @@
+---
+"docs": minor
+"web": minor
+"eslint-config-custom": minor
+"tsconfig": minor
+"ui": minor
+---
+
+init dp stack
--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,9 +1,9 @@
-import { Button } from "ui";
+import { Button } from 'ui';
 
 export default function Docs() {
   return (
     <div>
-      <h1>Docs</h1>
+      <h1>Dp Stack: Documentation</h1>
       <Button />
     </div>
   );
